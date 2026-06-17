bcee9e62e73ef0904ede5a81cb71d41b27163b3e
tangpeng <tangpeng@tangpengdeMacBook-Air.local>
2026-06-17 19:02:34 +0800

    fix

--- csrc/mc2/mega_moe/op_kernel/arch35/kernel_math_util.h
+++ csrc/mc2/mega_moe/op_kernel/arch35/kernel_math_util.h
@@ -1,23 +1,31 @@
 #ifndef MEGA_MOE_KERNEL_MATH_UTIL_H
 #define MEGA_MOE_KERNEL_MATH_UTIL_H
 
+#if defined(__ASCEND_C__) || defined(__CCE_KT_TEST__)
+#define MEGA_MOE_HOST_DEVICE __aicore__
+#else
+#define MEGA_MOE_HOST_DEVICE
+#endif
+
 namespace Ops { namespace Base {
+
 template <typename T>
-static constexpr T CeilAlign(T num, T align) {
+MEGA_MOE_HOST_DEVICE static constexpr T CeilAlign(T num, T align) {
     return ((num + align - 1) / align) * align;
 }
 template <typename T>
-static constexpr T CeilDiv(T num, T div) {
+MEGA_MOE_HOST_DEVICE static constexpr T CeilDiv(T num, T div) {
     return (num + div - 1) / div;
 }
 template <typename T>
-static constexpr T FloorAlign(T num, T align) {
+MEGA_MOE_HOST_DEVICE static constexpr T FloorAlign(T num, T align) {
     return (num / align) * align;
 }
 template <typename T>
-static constexpr T FloorDiv(T num, T div) {
+MEGA_MOE_HOST_DEVICE static constexpr T FloorDiv(T num, T div) {
     return num / div;
 }
+
 } }  // namespace Ops::Base
 
 #endif
